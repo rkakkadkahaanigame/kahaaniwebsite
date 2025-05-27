2450b836312977787fe9b691e05525112302893e
rkakkad <rkakkad@gmail.com>
2025-05-26 21:30:41 -0700

    Security: Move Firebase config to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuS4GoCSGuKQGCbNGA9Syf_ofynjETh0I",
-  authDomain: "game0222-c2cf2.firebaseapp.com",
-  projectId: "game0222-c2cf2",
-  storageBucket: "game0222-c2cf2.firebasestorage.app",
-  messagingSenderId: "772564806251",
-  appId: "1:772564806251:web:630e89b1709fa2e3af5f2e",
-  measurementId: "G-23CP1N3TRK"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
