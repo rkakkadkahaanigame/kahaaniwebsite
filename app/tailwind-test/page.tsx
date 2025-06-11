bad0d3f3a0f1021ccdb5215b7a8d1f4909d4c8b3
rkakkad <rkakkad@gmail.com>
2025-06-11 14:51:15 -0700

    Remove Tailwind test page

--- app/tailwind-test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TailwindTest() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-green-500 text-white text-4xl">
-      If you see a green background, Tailwind is working!
-    </div>
-  );
-} 
